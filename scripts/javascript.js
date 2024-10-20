5c9046b317cb78eb252ab40ee7888c5e464e89d1
SonderUA <umarbek.sonder@gmail.com>
2024-10-20 17:27:37 +0500

    feat: add eventListeners for keys and operators

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,17 +1,36 @@
 let calculator = {
 	PI: 3.1415,
 	add(a, b) {
-		return a + b;
+		return +a + +b;
 	},
 	subtract(a, b) {
-		return a - b;
+		return +a - +b;
 	},
 	multiply(a, b) {
-		return a * b;
+		return +a * +b;
 	},
 	divide(a, b) {
-		return a / b;
+		return +a / +b;
 	},
 };
 
-console.log(calculator.add(3, 5));
+let input = document.querySelector("#inputField");
+const keys = document.querySelectorAll(".key");
+const operators = document.querySelectorAll(".operator");
+for (let key of keys) {
+	key.addEventListener("click", (event) => {
+		if (event.target.id === "clear") {
+			input.textContent = "";
+		} else if (event.target.id === "enter") {
+			console.log(operate(input.textContent));
+		} else {
+			input.textContent += event.target.textContent;
+		}
+	});
+}
+
+for (let operator of operators) {
+	operator.addEventListener("click", (event) => {
+		input.textContent += ` ${event.target.textContent} `;
+	});
+}
