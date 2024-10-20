1e12c788b06ad46e977af7cf1a2b82ff9e11a917
SonderUA <umarbek.sonder@gmail.com>
2024-10-20 16:13:42 +0500

    feat: create a calculator object for calculations

---
+++ scripts/javascript.js
@@ -0,0 +1,17 @@
+let calculator = {
+	PI: 3.1415,
+	add(a, b) {
+		return a + b;
+	},
+	subtract(a, b) {
+		return a - b;
+	},
+	multiply(a, b) {
+		return a * b;
+	},
+	divide(a, b) {
+		return a / b;
+	},
+};
+
+console.log(calculator.add(3, 5));
