87075c0fe2a8d9d73aac9e96f1bdf6247bf9b22a
SonderUA <umarbek.sonder@gmail.com>
2024-10-20 17:27:56 +0500

    feat: create an operate function

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -14,6 +14,24 @@ let calculator = {
 	},
 };
 
+function operate(string) {
+	if (!string) {
+		alert("No input provided");
+		throw new Error("No input provided");
+	}
+	let [firstNum, operator, secondNum] = string.split(" ");
+	switch (operator) {
+		case "+":
+			return calculator.add(firstNum, secondNum);
+		case "-":
+			return calculator.subtract(firstNum, secondNum);
+		case "/":
+			return calculator.divide(firstNum, secondNum);
+		case "*":
+			return calculator.multiply(firstNum, secondNum);
+	}
+}
+
 let input = document.querySelector("#inputField");
 const keys = document.querySelectorAll(".key");
 const operators = document.querySelectorAll(".operator");
