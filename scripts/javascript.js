543da7b8c4489cf774108b9334c781761b520f7e
SonderUA <umarbek.sonder@gmail.com>
2024-10-22 19:09:05 +0500

    feat: add eventHandlers for keyboard and html button input

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -119,3 +119,103 @@ let display = document.querySelector(".content");
 let reminder = document.querySelector(".reminder");
 const digits = document.querySelectorAll(".key");
 const operators = document.querySelectorAll(".operator");
+
+for (let digit of digits) {
+	digit.addEventListener("click", (event) => {
+		let element = event.target;
+		if (DIGITS.includes(element.textContent)) {
+			if (!(display.textContent.length === 18)) {
+				// Prevent two zeros from being displayed
+				if (
+					display.textContent === "0" &&
+					!["0"].includes(element.textContent)
+				) {
+					calculator.clear(display, (empty = true));
+					display.textContent += element.textContent;
+				} else if (display.textContent !== "0") {
+					// Prevent two dots from being displayed
+					if (
+						!(
+							(display.textContent.match(/\./g) || []).length ===
+							1
+						) ||
+						element.textContent !== "."
+					)
+						display.textContent += element.textContent;
+				}
+			}
+		}
+		event.target.blur(); // Fix the always active bug on buttons
+	});
+}
+
+for (let operator of operators) {
+	operator.addEventListener("click", (event) => {
+		let element = event.target;
+		if (OPERATORS.includes(element.textContent)) {
+			calculator.calculate(element.textContent, display, reminder);
+		} else if (element.textContent === "Enter") {
+			calculator.calculate("=", display, reminder);
+		} else if (element.textContent === "←") {
+			if (display.textContent.length === 1) {
+				calculator.clear(display);
+			} else {
+				display.textContent = display.textContent.slice(
+					0,
+					display.textContent.length - 1
+				);
+			}
+		} else if (element.textContent === "C") {
+			calculator.clear(display);
+		} else if (element.textContent === "AC") {
+			calculator.power ? calculator.powerOff() : calculator.powerOn();
+		} else if (SOLO_OPERATORS.includes(element.textContent)) {
+			calculator.calculate(
+				element.textContent,
+				display,
+				reminder,
+				(solo = true)
+			);
+		}
+		event.target.blur();
+	});
+}
+
+document.addEventListener("keydown", (event) => {
+	let keyValue = event.key;
+	if (OPERATORS.includes(keyValue)) {
+		calculator.calculate(keyValue, display, reminder);
+	} else if (DIGITS.includes(keyValue)) {
+		if (!(display.textContent.length === 18)) {
+			if (display.textContent === "0" && !["0"].includes(keyValue)) {
+				calculator.clear(display, (empty = true));
+				display.textContent += keyValue;
+			} else if (display.textContent !== "0") {
+				if (
+					!((display.textContent.match(/\./g) || []).length === 1) ||
+					keyValue !== "."
+				)
+					display.textContent += keyValue;
+			}
+		}
+	} else if (keyValue === "Enter" || keyValue === "=") {
+		calculator.calculate("=", display, reminder);
+	} else if (keyValue === "Backspace") {
+		if (display.textContent.length === 1) {
+			calculator.clear(display);
+		} else {
+			display.textContent = display.textContent.slice(
+				0,
+				display.textContent.length - 1
+			);
+		}
+	} else if (keyValue === "Escape") {
+		calculator.clear(display);
+	} else if (SOLO_OPERATORS.includes(keyValue)) {
+		calculator.calculate(keyValue, display, reminder, (solo = true));
+	} else if (keyValue === "^") {
+		calculator.calculate("xy", display, reminder);
+	} else if (keyValue === "Meta") {
+		calculator.calculate("+/-", display, reminder, (solo = true));
+	}
+});
